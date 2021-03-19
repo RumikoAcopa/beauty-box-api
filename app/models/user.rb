70e532b2b0e2ecf918eb4d30dce04aef568d0330
rumikoacopa <rumikorios@gmail.com>
2021-03-19 00:13:51 -0400

    modify user relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :products
+  
   has_many :categories
+  has_many :products, through: :categories
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
