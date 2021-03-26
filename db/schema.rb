79ce6356f4fa6e6397760dabcbd0001901e3c7db
rumikoacopa <rumikorios@gmail.com>
2021-03-26 11:24:58 -0400

    modify schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_231755) do
+ActiveRecord::Schema.define(version: 2021_03_25_033856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,10 +49,8 @@ ActiveRecord::Schema.define(version: 2021_03_16_231755) do
     t.integer "quantity"
     t.string "details"
     t.bigint "user_id", null: false
-    t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
@@ -70,6 +68,5 @@ ActiveRecord::Schema.define(version: 2021_03_16_231755) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "categories", "users"
-  add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
 end
