517f02dd25f79c99569b8ebbde980c0ae0bda693
rumikoacopa <rumikorios@gmail.com>
2021-03-26 11:25:46 -0400

    modify has_many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
   has_many :categories
-  has_many :products, through: :categories
+  has_many :products
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
